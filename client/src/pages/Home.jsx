32ad218d7e74db7e414b1cc270eccd45535f1c78
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-10-04 10:00:21 -0700

    color pallette design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,11 @@ export default function Home() {
     return (
         <div>
             <HappyMan />
+            <div>
+                Insert some text here
+                with background color #CE902A
+                #FCEBB6
+            </div>
             <div className="wheel-div">
                 <Wheel />
             </div>
